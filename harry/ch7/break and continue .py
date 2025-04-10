b92891607d1f5e8c41e540fea0f209b352210aa2
musfirasarwar <musferasarwar@gmail.com>
2025-04-11 01:52:42 +0500

    Add files via upload

---
+++ harry/ch7/break and continue .py
@@ -0,0 +1,9 @@
+# for i in range (100):
+#     if (i== 23):
+#              break
+#     print (i)
+
+for i in range (100):
+    if (i==45):
+            continue
+    print (i)
---
+++ harry/ch7/pass.py
@@ -0,0 +1,7 @@
+for i in range (834):
+    pass
+
+
+while (i<455):
+    print (i)
+    i +=1
