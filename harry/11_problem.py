24465d253c536a17f80ff49e31569e6d6c46db46
musfirasarwar <musferasarwar@gmail.com>
2026-02-20 22:30:44 +0500

    Delete harry/11_problem.py

--- harry/11_problem.py
+++
@@ -1,6 +0,0 @@
-with open("c:/Learn-Python-main/harry/chapter 9 partice set/old.txt", "r") as f:
-        content=f.read()
-        
-with open("renamed_by_python.txt", "w") as f:
-    f.write(content)
-  
