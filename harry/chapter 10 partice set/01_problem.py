43b4a9db66836073e08061241666857683c8d9df
musfirasarwar <musferasarwar@gmail.com>
2026-02-21 22:11:50 +0500

    Add files via upload

---
+++ harry/chapter 10 partice set/01_problem.py
@@ -0,0 +1,13 @@
+class programmer:
+    company="microsoft"
+    def __init__(self,name,salary,pin):#duder method which is automatically called
+        self.name=name
+        self.pin=pin
+        self.salary=salary
+        
+p =programmer("musfira","123000","120000")
+print(p.name,p.pin,p.salary)
+
+p =programmer("anisa","123000","120000")
+print(p.name,p.pin,p.salary)
+
